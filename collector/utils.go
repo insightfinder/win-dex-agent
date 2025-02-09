e56b3838be2eb55a5e53ad5f9194f2429892c395
Maoyu <i@maoyu.wang>
2025-02-09 17:58:19 -0500

    clean up code

--- collector/utils.go
+++ collector/utils.go
@@ -1,16 +1,1 @@
 package collector
-
-import "fmt"
-
-func formatBytes(bytes uint64) string {
-	const unit = 1024
-	if bytes < unit {
-		return fmt.Sprintf("%d B", bytes)
-	}
-	div, exp := uint64(unit), 0
-	for n := bytes / unit; n >= unit; n /= unit {
-		div *= unit
-		exp++
-	}
-	return fmt.Sprintf("%.2f %cB", float64(bytes)/float64(div), "KMGTPE"[exp])
-}
